d03c89742a7b3340430465b824cb7e8de1e4f3ea
lori33c <lcha2@uci.edu>
2023-09-07 00:26:32 -0700

    connected db

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
 
 app = FastAPI()
 
+# Create a connection to the MongoDB database
+client = MongoClient('mongodb://localhost:27017')
+db = client['todolistdb']
+task = db['tasks'] #this is our task collection inside the db
+
+# Clear the existing tasks in the collection
+task.delete_many({})
+
+my_task = {
+    "name" : 'wash cat',
+    "description" : 'put soap on cat and hose down',
+    "rank" : 1
+}
+
+inserted_task = db.tasks.insert_one(my_task)
+
+
 @app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+def root():
+    return {"message" : "Hello, world!"}
